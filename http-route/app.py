b4b82dc291cbc31df3f8633a1534e723d7090bfd
Tony Yang <t510599@gmail.com>
2022-05-08 17:24:09 +0800

    [Add] http & route example

---
+++ http-route/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, url_for, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "<h1>Hello, World!</h1>"
+
+@app.route("/hello")
+def hello():
+    param = request.args
+    if param.get("name"):
+        return f"<h1>Hello, {param['name']}!</h1>"
+
+    return "<h1>Hello, <ruby>(　　)<rt>NO NAME</rt></ruby>!</h1>"
+
+@app.route("/get")
+def get():
+    return request.args
+
+@app.route("/card/<name>")
+def card(name):
+    return f"<h1>This is card for {name}!</h1>" + \
+            "<p>Welcome to sprout!</p>"
+
+@app.route("/echo", methods=["GET", "POST"])
+def form():
+    if request.method == "GET":
+        return f"""
+            <form action="{url_for('form')}" method="POST">
+                <input type="text" name="message">
+                <input type="submit" value="submit">
+            </form>
+        """
+    elif request.method == "POST":
+        return request.form.get("message")
+
+if __name__ == "__main__":
+    app.run(debug=True)
